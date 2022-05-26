a5c2cbc5ab8883f8f8e377057a5eda9865883c24
Caleb Norris <norrisc129@gmail.com>
2022-05-26 03:00:14 -0400

    Create Account Form

--- controllers/controller.php
+++ controllers/controller.php
@@ -3,5 +3,28 @@
 class Controller
 {
 
+    private $_f3;
+
+    function __construct($f3)
+    {
+        $this->_f3 = $f3;
+    }
+
+    function home()
+    {
+        $view = new Template();
+        echo $view->render('views/home.html');
+    }
+
+    function login() {
+        $view = new Template();
+        echo $view->render('views/login.html');
+    }
+
+    function createAccount() {
+        $view = new Template();
+        echo $view->render('views/createAccount.html');
+    }
+
 
 }
