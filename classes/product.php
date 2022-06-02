2a8d6b859501056ea513db7bbc3994761323f71c
Patrick Lindsay <lindsay.patrick@student.greenriver.edu>
2022-06-02 00:47:31 -0400

    Added constructor and get methods to Product class

--- classes/product.php
+++ classes/product.php
@@ -1,9 +1,61 @@
 <?php
 
+/**
+ * Product class container for basic product information.
+ * Parent class to Candle and Diffuser classes.
+ */
 class Product
 {
+    // FIELDS
     private $_productId;
     private $_productName;
     private $_productDescription;
     private $_productQTY;
+
+    // CONSTRUCTOR
+    function __construct($productId, $productName, $productDescription, $productQTY)
+    {
+        $this->_productId = $productId;
+        $this->_productName = $productName;
+        $this->_productDescription = $productDescription;
+        $this->_productQTY = $productQTY;
+    }
+
+    // GET METHODS
+
+    /**
+     * Method to get the unique product identifier used in the database
+     * @return int unique id of the product
+     */
+    function getProductId()
+    {
+        return $this->_productId;
+    }
+
+    /**
+     * Method to get the name of the product
+     * @return string name of the product
+     */
+    function getProductName()
+    {
+        return $this->_productName;
+    }
+
+    /**
+     * Method to get the full description of the product
+     * @return string detailed product description
+     */
+    function getProductDescription()
+    {
+        return $this->_productDescription;
+    }
+
+    /**
+     * Method to get the quantity of the product //TODO: Is this quantity in cart or quantity in stock
+     * @return int quantity of the product
+     */
+    function getProductQTY()
+    {
+        return $this->_productQTY;
+    }
 }
